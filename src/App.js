9796547ead8e8b2bf3c58a5b2ebf63bdb8769d2f
Jared Clyde <jarclyde@gmail.com>
2023-01-17 15:33:42 -0700

    part 6

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 	return (
 		<div className="App font-link">
 			<Header />
-			<OptionDisplay />
+			{currentDisplay ? <MainDisplay /> : <OptionDisplay />}
 		</div>
 	)
 }
--- src/Components/OptionDisplay.jsx
+++ src/Components/OptionDisplay.jsx
@@ -6,11 +6,24 @@ import { setDisplayCountry } from '../redux/slices/displayCountrySlice'
 
 const OptionDisplay = () => {
 	let currentPotentials = useSelector(selectPotentials)
+	console.log(currentPotentials)
+
+	const dispatch = useDispatch()
 
 	return (
 		<div className="stack">
 			{currentPotentials.map((cntry, idx) => {
-				return <h2 key={cntry.name.official}>{cntry.name.common}</h2>
+				return (
+					<h2
+						key={cntry.name.official}
+						className="country-option"
+						onClick={() => {
+							dispatch(setDisplayCountry(currentPotentials[idx]))
+						}}
+					>
+						{cntry.name.common}
+					</h2>
+				)
 			})}
 		</div>
 	)
